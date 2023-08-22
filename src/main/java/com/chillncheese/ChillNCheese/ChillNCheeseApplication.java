ff7acbefef0fcb42353aac8ef055f0bf9433b10b
mfavreau <mfavreau@cirilgroup.com>
2023-08-22 14:07:41 +0200

    mise en place du Back Spring, postgre

---
+++ src/main/java/com/chillncheese/ChillNCheese/ChillNCheeseApplication.java
@@ -0,0 +1,13 @@
+package com.chillncheese.ChillNCheese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillNCheeseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillNCheeseApplication.class, args);
+	}
+
+}
